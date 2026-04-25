4359ad13d3fe407c0cba175a2e77eb28cc5c19d3
Jose Ponce <joseponcegar@gmail.com>
2026-04-25 15:08:15 +0200

    feat: opcion de mostrar todas las filas de la tablas

--- irpf_spain.py
+++ irpf_spain.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 from pathlib import Path
 
+import pandas as pd
 from pandas import DataFrame
 
 from constantes import (
@@ -21,6 +22,9 @@ from tablas.transactions import leer_transactions
 
 
 def main(args: argparse.Namespace) -> None:  # noqa: C901, PLR0912, PLR0915
+
+    if args.all:
+        pd.options.display.max_rows = 10000
 
     if args.Account:
         account: DataFrame = leer_account(Path(args.Account))
@@ -124,44 +128,6 @@ def main(args: argparse.Namespace) -> None:  # noqa: C901, PLR0912, PLR0915
             pass
 
 
-parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument(
-    "-A",
-    "--Account",
-    help="Indica un archivo Account especifico",
-    default="./Account.csv",
-)
-parser.add_argument(
-    "-T",
-    "--Transactions",
-    help="Indica un archivo Transactions especifico",
-    default="./Transactions.csv",
-)
-parser.add_argument(
-    "-t",
-    "--tabla",
-    help="TABLA puede ser activos, divisas o posiciones",
-    default="activos",
-)
-parser.add_argument(
-    "-a",
-    "--agrupado",
-    help="Devuelve la tabla agrupada por productos y/o divisa",
-    action="store_true",
-)
-parser.add_argument("-y", "--year", type=int, help="Filtra por año los resultados")
-parser.add_argument("-m", "--month", type=int, help="Filtra por mes los resultados")
-parser.add_argument("-d", "--divisa", help="Filtra por divisa los resultados")
-parser.add_argument("-p", "--producto", help="Filtra por producto los resultados")
-parser.add_argument(
-    "-r",
-    "--rentaweb",
-    help="Introduce los resultados que hayas especificado en TABLA",
-    action="store_true",
-)
-args: argparse.Namespace = parser.parse_args()
-
-
 def filtro_anno(
     df: DataFrame,
     anno: int,
@@ -206,6 +172,49 @@ def filtro_agrupado(df: DataFrame, agrupar_por: list[str]) -> DataFrame:
             GANANCIA_PERDIDA,
         ]
     ].sum()
+
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument(
+    "-A",
+    "--Account",
+    help="Indica un archivo Account especifico",
+    default="./Account.csv",
+)
+parser.add_argument(
+    "-T",
+    "--Transactions",
+    help="Indica un archivo Transactions especifico",
+    default="./Transactions.csv",
+)
+parser.add_argument(
+    "-t",
+    "--tabla",
+    help="TABLA puede ser activos, divisas o posiciones",
+    default="activos",
+)
+parser.add_argument(
+    "-a",
+    "--agrupado",
+    help="Devuelve la tabla agrupada por productos y/o divisa",
+    action="store_true",
+)
+parser.add_argument("-y", "--year", type=int, help="Filtra por año los resultados")
+parser.add_argument("-m", "--month", type=int, help="Filtra por mes los resultados")
+parser.add_argument("-d", "--divisa", help="Filtra por divisa los resultados")
+parser.add_argument("-p", "--producto", help="Filtra por producto los resultados")
+parser.add_argument(
+    "-r",
+    "--rentaweb",
+    help="Introduce los resultados que hayas especificado en TABLA",
+    action="store_true",
+)
+parser.add_argument(
+    "--all",
+    help="Muestra todas las filas de la tabla",
+    action="store_true",
+)
+args: argparse.Namespace = parser.parse_args()
 
 
 main(args)
